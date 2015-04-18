065c45a2dd91aa70fb2e638704835487e68358a0
Mac Allen 2019 <mallen19@mac-allen-19.local>
2015-04-18 11:45:54 -0400

    show moods array

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,14 @@
 require 'bundler'
+
 Bundler.require
 
+require_relative "models/moods.rb"
 class MyApp < Sinatra::Base
 
   get '/' do
+  	@moods_arr = Moods.new.moods
+  	puts @moods_arr.class
+  	erb :index
 
   end
 
--- models/moods.rb
+++ models/moods.rb
@@ -1,1 +1,11 @@
-moods = ["angry", "depressed", "heartbreak-depressing", "heartbreak-feel good", "happy", "frustrated", "hyper", "inspired/motivated", "empathetic", "abandoned", "agitated", "aggressive", "anxious", "ashamed", "bitter", "ignorant", "cheerful", "compassionate", "conflicted", "cranky", "defeated", "hopeless", "defensive", "enraged", "sentimental", "pumped", "jealous", "impulsive", "irritated", "sassy", "victorious", "perky", "sorrowful", "repulsive", "unhappy", "sleepy"]
+# moods = ["angry", "depressed", "heartbreak-depressing", "heartbreak-feel good", "happy", "frustrated", "hyper/pumped", "inspired/motivated", "empathetic", "abandoned", "agitated", "aggressive", "anxious", "ashamed", "bitter", "ignorant", "cheerful", "compassionate", "conflicted", "cranky", "defeated", "hopeless", "defensive", "enraged", "sentimental", "jealous", "impulsive", "irritated", "sassy", "victorious", "perky", "sorrowful", "repulsive", "unhappy", "sleepy"]
+
+class Moods 
+	
+	attr_accessor :moods
+	
+	def initialize
+		
+		@moods = ["angry", "depressed", "heartbreak-depressing", "heartbreak-feel good", "happy", "frustrated/agitated", "hyper/pumped", "inspired/motivated", "empathetic", "abandoned", "aggressive", "anxious", "ashamed", "ignorant", "cheerful", "compassionate", "conflicted", "hopeless", "defensive", "enraged", "sentimental", "jealous", "impulsive", "sassy", "victorious", "perky", "sorrowful", "repulsive", "unhappy", "sleepy"]
+	end
+end
